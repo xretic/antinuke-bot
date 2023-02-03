2185af8f5035fbfbf70c317270d0a131552a180e
xretic <84147465+xretic@users.noreply.github.com>
2023-02-03 16:33:54 +0200

    Add files via upload

--- src/collectors/interactionCollector.ts
+++ src/collectors/interactionCollector.ts
@@ -38,7 +38,7 @@ export const interactionCollector = async (
 				}
 
 				const file = await import(
-					path.resolve(__dirname, `../actions/${type}/${i.customId}.ts`)
+					path.resolve(__dirname, `../actions/${type}/${i.customId}.{ts,js}`)
 				);
 
 				if (props) {
